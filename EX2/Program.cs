c797f5a111742a96b467608a4291ca5d047cec34
Anastasia <bonnyn7@yandex.ru>
2022-06-27 22:35:15 +0300

    Added ex1, ex2

---
+++ EX2/Program.cs
@@ -0,0 +1,5 @@
+﻿int N = new Random().Next (100, 1000);
+string str = N.ToString();
+char[] f = str.ToCharArray();
+Console.WriteLine (N+ "->" +  f[0].ToString ()+ f[2].ToString ());
+
--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите число 1: ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 2: ");
+int b = Convert.ToInt32 (Console.ReadLine ());
+int c= a%b;
+if (c==0) 
+{
+    Console.WriteLine ("Число {0} кратно числу {1}", a, b);
+}
+else
+{
+    Console.WriteLine ("Остаток от деления: " + c);
+}
